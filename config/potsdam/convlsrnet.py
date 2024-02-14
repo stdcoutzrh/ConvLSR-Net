2248072e26a5f2d9ec72154441849b724873c0ee
RenheZhang <stdcoutzrh@gmail.com>
2024-02-14 16:39:53 +0800

    Update convlsrnet.py

--- config/potsdam/convlsrnet.py
+++ config/potsdam/convlsrnet.py
@@ -17,7 +17,7 @@ accumulate_n = 1
 num_classes = len(CLASSES)
 classes = CLASSES
 
-weights_name = "0226-lsrformer-head88-down2-crop768"
+weights_name = "0226-convlsrnet-head88-down2-crop768"
 weights_path = "/home/zrh/datasets/semantic/potsdam/log_potsdam/{}".format(weights_name)
 test_weights_name = weights_name
 log_name = "/home/zrh/datasets/semantic/potsdam/log_potsdam/{}".format(weights_name)
@@ -32,7 +32,7 @@ pretrained_ckpt_path = None
 resume_ckpt_path = None
 
 #  define the network
-from geoseg.lsrformer.convlsrnet import Model
+from geoseg.model.convlsrnet import Model
 net = Model(n_class=num_classes)
 
 # define the loss
